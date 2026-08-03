ce558686084525edd35cd2a919b088644cc7975c
Ted Welch <ted.welch@biptec.com>
2026-08-03 10:17:40 +0000

    fix: install Caddy policy under plugin prefix

--- guest/caddy-policy/tests/test_policy.sh
+++ guest/caddy-policy/tests/test_policy.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ROOT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
-POLICY="$ROOT_DIR/src/usr/local/etc/caddy/caddy.d/10-acme-ca.global"
+POLICY="$ROOT_DIR/src/etc/caddy/caddy.d/10-acme-ca.global"
 EXPECTED='acme_ca https://acme-v02.api.letsencrypt.org/directory'
 
 [ -f "$POLICY" ]
